a71c2a8640fa3a4f9a7c7822e1fc300140125b32
pfot <157275217+pfot@users.noreply.github.com>
2024-01-21 20:13:29 +0300

    Add files via upload

---
+++ pr_git1/baza_ors.js
@@ -0,0 +1,16 @@
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 01t.jpg</i><b>при5 база орс 01t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 02t.jpg</i><b>при5 база орс 02t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 03t.jpg</i><b>при5 база орс 03t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 04t.jpg</i><b>при5 база орс 04t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 05t.jpg</i><b>при5 база орс 05t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 06t.jpg</i><b>при5 база орс 06t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 07t.jpg</i><b>при5 база орс 07t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 08t.jpg</i><b>при5 база орс 08t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 09t.jpg</i><b>при5 база орс 09t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 10t.jpg</i><b>при5 база орс 10t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 11t.jpg</i><b>при5 база орс 11t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 12t.jpg</i><b>при5 база орс 12t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 13t.jpg</i><b>при5 база орс 13t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 14t.jpg</i><b>при5 база орс 14t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 15t.jpg</i><b>при5 база орс 15t.jpg</b>');
+document.write('<i>https://raw.githubusercontent.com/pfot/fot/main/база_орс/при5 база орс 16t.jpg</i><b>при5 база орс 16t.jpg</b>');
